2b850f6929623325f2eb987b42ffe70a2dbca3f1
jelf <353742991@qq.com>
2018-04-25 02:19:34 +0800

    feat: 新增NavigationList、NavigationListItem组件

---
+++ components/Navigation/NavigationList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :class="prefixCls">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    const prefixCls = 'navigationList'
+
+    export default {
+        name: prefixCls,
+        props: {
+            prefixCls: {
+                type: String,
+                default: prefixCls
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "../../assets/styles/index";
+
+    $navigationPrefixCls: 'navigationList';
+    .#{$navigationPrefixCls} {
+        width: 200px;
+    }
+</style>
---
+++ components/Navigation/NavigationListItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :class="rootClasss">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    const prefixCls = 'navigationItem'
+
+    export default {
+        name: prefixCls,
+        props: {
+            prefixCls: {
+                type: String,
+                default: prefixCls
+            },
+            active: Boolean
+        },
+        computed: {
+            rootClasss() {
+                return [
+                    this.prefixCls,
+                    {[`${this.prefixCls}-active`]: this.active}
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    @import "../../assets/styles/index";
+
+    $navigationPrefixCls: 'navigationItem';
+    .#{$navigationPrefixCls} {
+        line-height: 3.5;
+        padding-left: 20px;
+        color: #FFF;
+        cursor: pointer;
+        &-active {
+            color: #18CCB1;
+            background-color: #FFF;
+            border-top-left-radius: 8px;
+            border-bottom-left-radius: 8px;
+        }
+    }
+</style>
